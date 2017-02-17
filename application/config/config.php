d367a0d3dd2def5416c9199cfce22572507a40e7
samer <mrsamer123@hotmail.com>
2017-02-17 13:36:45 +0000

    Added pages and routes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ciblog';
+$config['base_url'] = 'https://cavalier-mrsamer.c9users.io/projects/ciblog';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/ciblog';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
